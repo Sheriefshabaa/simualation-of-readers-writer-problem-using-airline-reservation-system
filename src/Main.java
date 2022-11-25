4e1bb0505b6a9bbb762537d134d5beb869ba1c1f
sheriefshabaa <shekoshabban@gmail.com>
2022-11-25 19:51:52 +0200

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import ReadersAndWriters.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ReadersAndWriters reader = new ReadersAndWriters();
     }
 }
