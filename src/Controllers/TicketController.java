6b2154a43123826a534612caac96bc621258f457
sheriefshabaa <shekoshabban@gmail.com>
2022-12-17 16:33:03 +0200

    init

--- src/Controllers/TicketController.java
+++ src/Controllers/TicketController.java
@@ -1,4 +1,18 @@
 package Controllers;
 import Models.*;
-public class TicketController {
+
+import java.util.Random;
+
+public class TicketController extends Ticket {
+    Ticket ticket  = new Ticket();
+    int[] reservedTickets = new int[Flight.getTotal_seats()];
+    Random ticketRandomizer = new Random();
+
+    private void createTicket(){
+        ticket.setTicketId(ticketRandomizer.nextInt(1000,5000));
+    }
+    public void tickerInitiator(){
+
+    }
+
 }
