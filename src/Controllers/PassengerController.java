d05f836bf23f0129fe52e31af521061b9feb78da
sheriefshabaa <shekoshabban@gmail.com>
2022-12-19 10:02:51 +0200

    init

--- src/Controllers/PassengerController.java
+++ src/Controllers/PassengerController.java
@@ -16,6 +16,7 @@ public class PassengerController extends Passenger implements Runnable {
     Passenger pass = new Passenger();//object to be changed after each creation
     TicketController ticketController = new TicketController();
     FlightDate flightDate = new FlightDate();
+    int totalSeats;
     private int seatsRandomizer (){ // this functions manages the randomness of created seats at each thread.
         int requestedSeats;
         requestedSeats = seatsRandomizer.nextInt(1,4);
@@ -37,23 +38,22 @@ public class PassengerController extends Passenger implements Runnable {
         currentFlight.setFlight_id();
         delayedFlight.setFlight_id();
         flightDate.setThisDate();
-        int totalSeats = Flight.getTotal_seats();
+         totalSeats = Flight.getTotal_seats();
         int currentFlightSeatsNumber = 0;
         int delayedFlightSeatsNumber = 0;
         // seats that will be assigned for all flight
-        System.out.println("Passenger name\t\tTicket ID\tPhone\tFlight status\tSeat Number\tFlight ID\tAir line\t\tdeparture\t\t\t Landing\t\t\t  Duration\t\tTake-off Date");
-        while (totalSeats > 0) {
+        while (totalSeats >= 0) {
             int bookedSeats = seatsRandomizer();
-                if (bookedSeats>totalSeats){
-                    flightDate.setThisDate();
+              if (bookedSeats>totalSeats){
+                    //flightDate.setThisDate();
                     for (int i = 1; i <= bookedSeats; i++) {
                         delayedFlightSeatsNumber++;
                         passengerInitiator();
                         pass.setFlightStatus(ticketStatus[1]);
-                        /*System.out.print(pass.getFirstName() + " " + pass.getLastName() + "  \t\t" + ticketController.getTicketId()+
+                        System.out.print(pass.getFirstName() + " " + pass.getLastName() + "  \t\t" + ticketController.getTicketId()+
                                 "  \t\t"+pass.getPhone() + "  \t\t" + pass.getFlightStatus() + "\t\t\t" + delayedFlightSeatsNumber + "\t\t" +
                                 delayedFlight.getFlight_id() + "\t\t" + delayedFlight.getAirLine() + "\t\t" + delayedFlight.getFrom_loc() + "\t\t" +
-                                delayedFlight.getTo_loc() + "\t\t" + delayedFlight.getDuration() +flightDate.getThisDate() +"\t\t"+"\n");*/
+                                delayedFlight.getTo_loc() + "\t\t" + delayedFlight.getDuration() +"\t\t"+flightDate.getThisDate() +"\t\t"+"\n");
                     }
                 }
                 else {
@@ -62,14 +62,16 @@ public class PassengerController extends Passenger implements Runnable {
                         currentFlightSeatsNumber++;
                         passengerInitiator();
                         pass.setFlightStatus(ticketStatus[0]);
-                        /*System.out.print(pass.getFirstName() + " " + pass.getLastName() + "  \t\t" + ticketController.getTicketId()+
+                        System.out.print(pass.getFirstName() + " " + pass.getLastName() + "  \t\t" + ticketController.getTicketId()+
                                 "  \t\t"+pass.getPhone() + "  \t\t" + pass.getFlightStatus() + "\t\t\t" + currentFlightSeatsNumber + "\t\t" +
                                 currentFlight.getFlight_id() + "\t\t" + currentFlight.getAirLine() + "\t\t" + currentFlight.getFrom_loc() + "\t\t" +
-                                currentFlight.getTo_loc() + "\t\t" + currentFlight.getDuration() +"\t\t"+flightDate.getThisDate() +"\t\t"+"\n");*/
+                                currentFlight.getTo_loc() + "\t\t" + currentFlight.getDuration() +"\t\t"+flightDate.getThisDate() +"\t\t"+"\n");
 
                     }
                 }
-
+                if (totalSeats == 0){
+                    totalSeats = -1;
+                }
             }
 
         }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,42 @@
 import ReadersAndWriters.*;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import Models.*;
+import Controllers.*;
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+    public static void main(String[] args) throws Exception {
+        //ExecutorService executor = Executors.newFixedThreadPool(3);
+        //prompts
+        System.out.println("Hello Admin!\nThis Program is an Airline reservation System Using Multithreading.");
+        System.out.println("First we need to setup some configuration to start the execution.");
+        //Variables&Objects
+        PassengerController passengerController = new PassengerController();
+        FlightController flightController= new FlightController();
+        TicketController ticketController = new TicketController();
+        FlightController currentFlight = new FlightController();
+        FlightController delayedFlight = new FlightController() ;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please Enter the intended Number of Sets for This Flight : ");
+        int TheWholeFlightCapacity = input.nextInt();
+        System.out.print("That's Really Great; can you provide me one more information about the individual flight size :");
+        int individualFlightCapacity = input.nextInt();
+        System.out.println("Thanks! now let's start the execution...");
+        int numberOfCreatedFlights = flightController.flightInfo(TheWholeFlightCapacity,individualFlightCapacity);
+        System.out.println("the number of flights is : "+numberOfCreatedFlights
+                +" each flight consist of "+individualFlightCapacity+" Seats");
+        System.out.println("the overflow Seats are : "+flightController.getOverFlowSeats()+" These seats will be reserved independently after finishing the available flights.");
+        System.out.println("Passenger name\t\tTicket ID\tPhone\tFlight status\tSeat Number\tFlight ID\tAir line\t\tdeparture\t\t\t Landing\t\t\t  Duration\t\tTake-off Date");
+            currentFlight.setFlight_id();
+            delayedFlight.setFlight_id();
+        for (int i = 0; i < individualFlightCapacity; i++) {
+            passengerController.createPassenger();
+        }
+
+    }
+
+    @Override
+    public void run() {
+
     }
 }
